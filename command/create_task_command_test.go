d89826b04158b0ebefcd6dac4112ecae74ff6437
Ashish Kumar <ashish.k@mindfiresolutions.com>
2021-09-09 19:19:48 +0530

    Added more create task command test.

--- command/create_task_command_test.go
+++ command/create_task_command_test.go
@@ -2,6 +2,7 @@ package command
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -13,4 +14,19 @@ func TestCreateTaskCommandByStreamContent(t *testing.T) {
 
 	assert.Nil(t, err)
 	assert.IsType(t, &CreateTaskCommand{}, command)
+	assert.Equal(t, "8419c611-d384-4e84-8114-23a05328e23b", command.Uuid)
+	assert.Equal(t, "This is a new Task", command.Description)
+	assert.Equal(t, "2021-10-02T15:04:05+07:00", command.DueAt.Format(time.RFC3339))
+}
+
+func TestCreateTaskCommandByStreamContentWithoutUuid(t *testing.T) {
+	content := []byte(`{"description":"This is a new Task","dueAt":"2021-10-02T15:04:05+07:00"}`)
+
+	command, err := CreateTaskCommandByStreamContent(content)
+
+	assert.Nil(t, err)
+	assert.IsType(t, &CreateTaskCommand{}, command)
+	assert.NotEmpty(t, command.Uuid)
+	assert.Equal(t, "This is a new Task", command.Description)
+	assert.Equal(t, "2021-10-02T15:04:05+07:00", command.DueAt.Format(time.RFC3339))
 }
